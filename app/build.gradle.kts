77f4c8a4bfd1f0c3b53048ab6953680762c0e5cc
docmalong2024-prog <docmalong2024@gmail.com>
2026-08-19 15:33:41 +0700

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,12 @@
+android {
+    namespace = "com.adrvision.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.adrvision.app"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
